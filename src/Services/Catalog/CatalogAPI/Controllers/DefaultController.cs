097aada02772ffcf8e9859cca05191ac07b537d3
molinaja <juan.molina.cr@gmail.com>
2021-02-17 00:12:37 -0600

    Primera carga codigo

---
+++ src/Services/Catalog/CatalogAPI/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class defaultController : ControllerBase
+    {
+
+        private readonly ILogger<defaultController> _logger;
+
+        public defaultController(ILogger<defaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Running.... Default";
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogDomain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogDomain
+{
+    public class Product
+    {
+        public int productID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public ProductInStock stock { get; set; }
+
+    }
+}
